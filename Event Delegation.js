2bc33cbedfc1787118cd654b3f4c94d17b0e13c3
PranshuKumar <pranu92@gmail.com>
2016-07-26 20:46:53 -0700

    Event Delegation

--- Event Delegation.js
+++ Event Delegation.js
@@ -1,1 +1,11 @@
-Event Delegation 
+// Event Delegation 
+
+// JavaScript event delegation is a simple technique by which you add a single event handler to a parent element in order to avoid having to add event handlers to multiple child elements.
+
+// How will it help me?
+// Imagine an HTML table with 10 columns and 100 rows in which you want something to happen when the user clicks on a table cell. 
+// For example, I once had to make each cell of a table of that size editable when clicked. 
+// Adding event handlers to each of the 1000 cells would be a major performance problem and, potentially, a source of browser-crashing memory leaks.
+// Instead, using event delegation, you would add only one event handler to the table element, intercept the click event and determine which cell was clicked.
+
+
