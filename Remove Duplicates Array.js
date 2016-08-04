6838a3f4916eb21a9da44ccc1dd189c78aac1498
PranshuKumar <pranu92@gmail.com>
2016-08-03 23:10:28 -0700

    Remove Array Duplicates

--- Remove Duplicates Array.js
+++ Remove Duplicates Array.js
@@ -1,6 +1,6 @@
 // Remove Duplicates From an Array 
 
-Methos 1 - 
+Method 1 - 
 
 var uniq = array.filter(function(item,pos){
 	return array.indexOf(item)==pos;
@@ -9,4 +9,62 @@ var uniq = array.filter(function(item,pos){
 
 
 Method 2 -
+
+function uniq(array){
+	var seen = {};
+	return array.filter(function(item){
+		return seen.hasOwnProperty(item) ? false : (seen[item]=true);
+	});
+}
+
+
+Method 3 -
+
+function uniq(a) {
+    return a.sort().filter(function(item, pos, ary) {
+        return !pos || item != ary[pos - 1];
+    })
+}
+
+
+
+Method 4 -
+
+function uniq(arr){
+    var map={};
+    var newArr =[];
+    for(var i=0;i<arr.length;i++){
+        if(map.hasOwnProperty(arr[i])) continue;
+        else{   
+               map[arr[i]]=1;
+               newArr.push(arr[i]);
+        }       
+    }
+    return newArr;
+}
+
+
+function uniq_fast(a) {
+    var seen = {};
+    var out = [];
+    var len = a.length;
+    var j = 0;
+    for(var i = 0; i < len; i++) {
+         var item = a[i];
+         if(seen[item] !== 1) {
+               seen[item] = 1;
+               out[j++] = item;
+         }
+    }
+    return out;
+}
+
+
+
+Method 5 -
+
+In ES6
+
+var uniq = [ ...new Set(array) ];
+
 
